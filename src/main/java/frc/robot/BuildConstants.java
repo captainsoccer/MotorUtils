18ad505f5e8ac47f04f7af5fc0ef6a9ba97fb233
captainsoccer <justeyallevi@gmail.com>
2025-05-23 18:45:34 +0300

    made things nicer and added docs

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "MotorUtils";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 51;
-  public static final String GIT_SHA = "a9f56ad76c701f7be49eaf0d5414bcb755e736ac";
-  public static final String GIT_DATE = "2025-05-23 07:57:15 EDT";
+  public static final int GIT_REVISION = 52;
+  public static final String GIT_SHA = "d6c3b97cda928d9b8c259d65058531cb019c3d5a";
+  public static final String GIT_DATE = "2025-05-23 08:03:15 EDT";
   public static final String GIT_BRANCH = "master";
-  public static final String BUILD_DATE = "2025-05-23 08:02:49 EDT";
-  public static final long BUILD_UNIX_TIME = 1748001769279L;
+  public static final String BUILD_DATE = "2025-05-23 11:35:57 EDT";
+  public static final long BUILD_UNIX_TIME = 1748014557511L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
--- src/main/java/util/BasicMotor/LogFrame.java
+++ src/main/java/util/BasicMotor/LogFrame.java
@@ -7,119 +7,158 @@ import util.BasicMotor.Measurements.Measurements;
 
 @AutoLog
 public class LogFrame {
-  public ControllerFrame controllerFrame;
-  public SensorData sensorData;
-  public Measurements.Measurement measurement;
+    /**
+     * the latest frame of the controller
+     */
+    public ControllerFrame controllerFrame;
+    /**
+     * the latest sensor data of the motor
+     */
+    public SensorData sensorData;
+    /**
+     * the latest measurement of the motor
+     */
+    public Measurements.Measurement measurement;
 
-  public record SensorData(
-      double temperature,
-      double currentDraw,
-      double currentOutput,
-      double voltageOutput,
-      double voltageInput,
-      double powerDraw,
-      double powerOutput,
-      double dutyCycle,
-      String faults) {}
-
-  public record PIDOutput(double pOutput, double iOutput, double dOutput, double totalOutput) {
-    public PIDOutput() {
-      this(0, 0, 0, 0);
-    }
-  }
-
-  public record FeedForwardOutput(
-      double simpleFeedForward,
-      double frictionFeedForward,
-      double kV,
-      double calculatedFeedForward,
-      double totalOutput) {
-    public FeedForwardOutput() {
-      this(0, 0, 0, 0, 0);
-    }
-
-    public FeedForwardOutput(
-        double simpleFeedForward,
-        double frictionFeedForward,
-        double kV,
-        double calculatedFeedForward) {
-      this(
-          simpleFeedForward,
-          frictionFeedForward,
-          kV,
-          calculatedFeedForward,
-          simpleFeedForward + frictionFeedForward + kV + calculatedFeedForward);
-    }
-  }
-
-  /**
-   * The frame of the controller
-   *
-   * @param totalOutput the total output of the controller (in volts)
-   * @param pidOutput the PID output of the controller (not always used)
-   * @param feedForwardOutput the feedforward output of the controller
-   * @param setpoint the setpoint of the controller in units of measurement
-   * @param measurement the measurement of the controller in units of measurement
-   * @param error the error of the controller in units of measurement
-   * @param goal the goal of the controller in units of measurement (used when using a profile)
-   * @param mode the mode of the controller (position, velocity, voltage, percent output)
-   */
-  public record ControllerFrame(
-      double totalOutput,
-      PIDOutput pidOutput,
-      FeedForwardOutput feedForwardOutput,
-      double setpoint,
-      double measurement,
-      double error,
-      double goal,
-      Controller.RequestType mode) {
-
-    /** empty constructor for the controller frame used when stopping the motor */
-    public ControllerFrame() {
-      this(0, new PIDOutput(), new FeedForwardOutput(), 0, 0, 0, 0, Controller.RequestType.STOP);
+    /**
+     * the record holding the sensor data of the motor
+     * @param temperature the temperature of the motor (in celsius)
+     * @param currentDraw the current draw of the motor (in amps)
+     * @param currentOutput the current output of the motor (in amps)
+     * @param voltageOutput the voltage output of the motor (in volts)
+     * @param voltageInput the voltage input of the motor (in volts)
+     * @param powerDraw the power draw of the motor (in watts)
+     * @param powerOutput the power output of the motor (in watts)
+     * @param dutyCycle the duty cycle of the motor (0-1)
+     * @param faults the faults of the motor (not supported by all motor controllers)
+     */
+    public record SensorData(
+            double temperature,
+            double currentDraw,
+            double currentOutput,
+            double voltageOutput,
+            double voltageInput,
+            double powerDraw,
+            double powerOutput,
+            double dutyCycle,
+            String faults) {
     }
 
     /**
-     * apply the pid output to the controller frame
-     *
-     * @param frame the controller frame to apply the pid output to
-     * @param output the pid output to apply to the controller frame
+     * the record holding the PID output of the controller
+     * @param pOutput the P output of the controller (in volts)
+     * @param iOutput the I output of the controller (in volts)
+     * @param dOutput the D output of the controller (in volts)
+     * @param totalOutput the total output of the controller (in volts)
      */
-    public ControllerFrame(ControllerFrame frame, PIDOutput output) {
-      this(
-          frame.totalOutput + output.totalOutput,
-          output,
-          frame.feedForwardOutput,
-          frame.setpoint,
-          frame.measurement,
-          frame.error,
-          frame.goal,
-          frame.mode);
+    public record PIDOutput(double pOutput, double iOutput, double dOutput, double totalOutput) {
+        public PIDOutput() {
+            this(0, 0, 0, 0);
+        }
     }
 
     /**
-     * apply the feedforward output to the controller frame
-     *
-     * @param frame the controller frame to apply the feedforward output to
-     * @param output the feedforward output to apply to the controller frame
-     * @param constraint apply the constraints to the output (clamping and deadband)
+     * the record holding the feedforward output of the controller
+     * @param simpleFeedForward the simple feedforward output of the controller (in volts)
+     * @param frictionFeedForward the friction feedforward output of the controller (in volts)
+     * @param kV the kV output of the controller (in volts)
+     * @param calculatedFeedForward the calculated feedforward output of the controller (in volts)
+     * @param totalOutput the total output of the controller (in volts)
      */
-    public ControllerFrame(
-        ControllerFrame frame, PIDOutput output, ControllerConstrains constraint) {
-      this(
-          constraint.checkMotorOutput(frame.totalOutput + output.totalOutput),
-          output,
-          frame.feedForwardOutput,
-          frame.setpoint,
-          frame.measurement,
-          frame.error,
-          frame.goal,
-          frame.mode);
+    public record FeedForwardOutput(
+            double simpleFeedForward,
+            double frictionFeedForward,
+            double kV,
+            double calculatedFeedForward,
+            double totalOutput) {
+        public FeedForwardOutput() {
+            this(0, 0, 0, 0, 0);
+        }
+
+        public FeedForwardOutput(
+                double simpleFeedForward,
+                double frictionFeedForward,
+                double kV,
+                double calculatedFeedForward) {
+            this(
+                    simpleFeedForward,
+                    frictionFeedForward,
+                    kV,
+                    calculatedFeedForward,
+                    simpleFeedForward + frictionFeedForward + kV + calculatedFeedForward);
+        }
     }
 
-    public ControllerFrame(
-        double output, double wanted, double measurement, Controller.RequestType mode) {
-      this(output, new PIDOutput(), new FeedForwardOutput(), wanted, measurement, 0, 0, mode);
+    /**
+     * The frame of the controller
+     *
+     * @param totalOutput       the total output of the controller (in volts)
+     * @param pidOutput         the PID output of the controller (not always used)
+     * @param feedForwardOutput the feedforward output of the controller
+     * @param setpoint          the setpoint of the controller in units of measurement
+     * @param measurement       the measurement of the controller in units of measurement
+     * @param error             the error of the controller in units of measurement
+     * @param goal              the goal of the controller in units of measurement (used when using a profile)
+     * @param mode              the mode of the controller (position, velocity, voltage, percent output)
+     */
+    public record ControllerFrame(
+            double totalOutput,
+            PIDOutput pidOutput,
+            FeedForwardOutput feedForwardOutput,
+            double setpoint,
+            double measurement,
+            double error,
+            double goal,
+            Controller.RequestType mode) {
+
+        /**
+         * empty constructor for the controller frame used when stopping the motor
+         */
+        public ControllerFrame() {
+            this(0, new PIDOutput(), new FeedForwardOutput(), 0, 0, 0, 0, Controller.RequestType.STOP);
+        }
+
+        /**
+         * apply the pid output to the controller frame
+         *
+         * @param frame  the controller frame to apply the pid output to
+         * @param output the pid output to apply to the controller frame
+         */
+        public ControllerFrame(ControllerFrame frame, PIDOutput output) {
+            this(
+                    frame.totalOutput + output.totalOutput,
+                    output,
+                    frame.feedForwardOutput,
+                    frame.setpoint,
+                    frame.measurement,
+                    frame.error,
+                    frame.goal,
+                    frame.mode);
+        }
+
+        /**
+         * apply the feedforward output to the controller frame
+         *
+         * @param frame      the controller frame to apply the feedforward output to
+         * @param output     the feedforward output to apply to the controller frame
+         * @param constraint apply the constraints to the output (clamping and deadband)
+         */
+        public ControllerFrame(
+                ControllerFrame frame, PIDOutput output, ControllerConstrains constraint) {
+            this(
+                    constraint.checkMotorOutput(frame.totalOutput + output.totalOutput),
+                    output,
+                    frame.feedForwardOutput,
+                    frame.setpoint,
+                    frame.measurement,
+                    frame.error,
+                    frame.goal,
+                    frame.mode);
+        }
+
+        public ControllerFrame(
+                double output, double wanted, double measurement, Controller.RequestType mode) {
+            this(output, new PIDOutput(), new FeedForwardOutput(), wanted, measurement, 0, 0, mode);
+        }
     }
-  }
 }
