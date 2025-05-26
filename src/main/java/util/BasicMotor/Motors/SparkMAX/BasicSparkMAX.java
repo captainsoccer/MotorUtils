a1700553d0e7a9a82bb11265f9e0849e55e06d52
captainsoccer <justeyallevi@gmail.com>
2025-05-26 16:51:22 +0300

    made it nicer

--- src/main/java/util/BasicMotor/Motors/SparkMAX/BasicSparkMAX.java
+++ src/main/java/util/BasicMotor/Motors/SparkMAX/BasicSparkMAX.java
@@ -18,160 +18,159 @@ import util.BasicMotor.Measurements.RevEncoders.MeasurementsREVRelative;
 import util.BasicMotor.MotorManager;
 
 public class BasicSparkMAX extends BasicMotor {
-  private final SparkMax motor;
-  private final SparkMaxConfig config;
+    private final SparkMax motor;
+    private final SparkMaxConfig config;
 
-  public BasicSparkMAX(
-      ControllerGains gains,
-      int id,
-      boolean isBrushless,
-      String name,
-      double gearRatio,
-      MotorManager.ControllerLocation location) {
+    public BasicSparkMAX(
+            ControllerGains gains,
+            int id,
+            boolean isBrushless,
+            String name,
+            double gearRatio,
+            MotorManager.ControllerLocation location) {
 
-    super(gains, name, location);
+        super(gains, name, location);
 
-    this.motor =
-        new SparkMax(
-            id,
-            isBrushless ? SparkLowLevel.MotorType.kBrushless : SparkLowLevel.MotorType.kBrushed);
+        this.motor =
+                new SparkMax(
+                        id,
+                        isBrushless ? SparkLowLevel.MotorType.kBrushless : SparkLowLevel.MotorType.kBrushed);
 
-    this.config = new SparkMaxConfig();
-    config.voltageCompensation(
-        MotorManager.motorIdleVoltage); // set the voltage compensation to the idle voltage
-    // all configs should be stored in code and not on motor
-    applyConfig();
+        this.config = new SparkMaxConfig();
+        config.voltageCompensation(
+                MotorManager.motorIdleVoltage); // set the voltage compensation to the idle voltage
+        // all configs should be stored in code and not on motor
+        applyConfig();
 
-    setMeasurements(new MeasurementsREVRelative(motor.getEncoder(), gearRatio));
-  }
-
-  @Override
-  protected void updatePIDGainsToMotor(PIDGains pidGains) {
-    config.closedLoop.pid(pidGains.getK_P(), pidGains.getK_I(), pidGains.getK_D());
-    config.closedLoop.iZone(pidGains.getI_Zone());
-    config.closedLoop.iMaxAccum(pidGains.getI_MaxAccum());
-
-    applyConfig();
-  }
-
-  @Override
-  protected void updateConstraints(ControllerConstrains constraints) {
-    config.closedLoop.maxOutput(constraints.getMaxMotorOutput() / MotorManager.motorIdleVoltage);
-    config.closedLoop.minOutput(constraints.getMinMotorOutput() / MotorManager.motorIdleVoltage);
-
-    if (constraints.getConstraintType() == ControllerConstrains.ConstraintType.LIMITED) {
-      config.softLimit.forwardSoftLimit(constraints.getMaxValue());
-      config.softLimit.reverseSoftLimit(constraints.getMinValue());
-      config.softLimit.forwardSoftLimitEnabled(true);
-      config.softLimit.reverseSoftLimitEnabled(true);
-    } else {
-      config.softLimit.forwardSoftLimitEnabled(false);
-      config.softLimit.reverseSoftLimitEnabled(false);
+        setMeasurements(new MeasurementsREVRelative(motor.getEncoder(), gearRatio));
     }
 
-    applyConfig();
-  }
+    @Override
+    protected void updatePIDGainsToMotor(PIDGains pidGains) {
+        config.closedLoop.pid(pidGains.getK_P(), pidGains.getK_I(), pidGains.getK_D());
+        config.closedLoop.iZone(pidGains.getI_Zone());
+        config.closedLoop.iMaxAccum(pidGains.getI_MaxAccum());
 
-  @Override
-  protected void setMotorOutput(double setpoint, double feedForward, Controller.RequestType mode) {
-    // TODO: add error code handling for the motor controller
-    switch (mode) {
-        // stop the motor output
-      case STOP -> stopMotorOutput();
-        // set the motor output to a voltage
-      case VOLTAGE -> motor.setVoltage(setpoint);
-        // set the motor output to a duty cycle
-      case PRECENT_OUTPUT -> motor.set(setpoint);
-        // set the motor output to a position
-      case POSITION, PROFILED_POSITION -> setClosedLoopOutput(
-          setpoint, feedForward, SparkBase.ControlType.kPosition);
-        // set the motor output to a velocity
-      case VELOCITY, PROFILED_VELOCITY -> setClosedLoopOutput(
-          setpoint, feedForward, SparkBase.ControlType.kVelocity);
+        applyConfig();
     }
-  }
 
-  /**
-   * sets the closed loop output of the motor
-   *
-   * @param setpoint the setpoint of the closed loop output
-   * @param feedForward the feed forward of the closed loop output
-   * @param mode the control type of the closed loop output
-   */
-  private void setClosedLoopOutput(
-      double setpoint, double feedForward, SparkBase.ControlType mode) {
-    motor
-        .getClosedLoopController()
-        .setReference(setpoint, mode, ClosedLoopSlot.kSlot0, feedForward);
-  }
+    @Override
+    protected void updateConstraints(ControllerConstrains constraints) {
+        config.closedLoop.maxOutput(constraints.getMaxMotorOutput() / MotorManager.motorIdleVoltage);
+        config.closedLoop.minOutput(constraints.getMinMotorOutput() / MotorManager.motorIdleVoltage);
 
-  @Override
-  protected void stopMotorOutput() {
-    motor.stopMotor();
-  }
+        if (constraints.getConstraintType() == ControllerConstrains.ConstraintType.LIMITED) {
+            config.softLimit.forwardSoftLimit(constraints.getMaxValue());
+            config.softLimit.reverseSoftLimit(constraints.getMinValue());
+            config.softLimit.forwardSoftLimitEnabled(true);
+            config.softLimit.reverseSoftLimitEnabled(true);
+        } else {
+            config.softLimit.forwardSoftLimitEnabled(false);
+            config.softLimit.reverseSoftLimitEnabled(false);
+        }
 
-  @Override
-  protected LogFrame.SensorData getSensorData() {
-    return null;
-  }
+        applyConfig();
+    }
 
-  @Override
-  protected LogFrame.PIDOutput getPIDLatestOutput() {
-    return LogFrame.PIDOutput.EMPTY;
-  }
-
-  @Override
-  public void setCurrentLimits(CurrentLimits currentLimits) {
-    // TODO: create a rev current limits class
-  }
-
-  @Override
-  public void setIdleMode(IdleMode mode) {
-    SparkBaseConfig.IdleMode value =
+    @Override
+    protected void setMotorOutput(double setpoint, double feedForward, Controller.RequestType mode) {
+        // TODO: add error code handling for the motor controller
         switch (mode) {
-          case BRAKE -> SparkBaseConfig.IdleMode.kBrake;
-          case COAST -> SparkBaseConfig.IdleMode.kCoast;
-        };
-
-    config.idleMode(value);
-    applyConfig();
-  }
-
-  @Override
-  protected void setMotorPosition(double position) {
-    if (!(getMeasurements() instanceof MeasurementsREV encoder)) {
-      // TODO: add alert to say the encoder is not a rev encoder anymore
-      return;
+            // stop the motor output
+            case STOP -> stopMotorOutput();
+            // set the motor output to a voltage
+            case VOLTAGE -> motor.setVoltage(setpoint);
+            // set the motor output to a duty cycle
+            case PRECENT_OUTPUT -> motor.set(setpoint);
+            // set the motor output to a position
+            case POSITION, PROFILED_POSITION -> setClosedLoopOutput(
+                    setpoint, feedForward, SparkBase.ControlType.kPosition);
+            // set the motor output to a velocity
+            case VELOCITY, PROFILED_VELOCITY -> setClosedLoopOutput(
+                    setpoint, feedForward, SparkBase.ControlType.kVelocity);
+        }
     }
 
-    encoder.setEncoderPosition(position);
+    /**
+     * sets the closed loop output of the motor
+     *
+     * @param setpoint    the setpoint of the closed loop output
+     * @param feedForward the feed forward of the closed loop output
+     * @param mode        the control type of the closed loop output
+     */
+    private void setClosedLoopOutput(double setpoint, double feedForward, SparkBase.ControlType mode) {
+        motor.getClosedLoopController().setReference(setpoint, mode, ClosedLoopSlot.kSlot0, feedForward);
+    }
 
-    motor.getClosedLoopController().setIAccum(0);
-  }
+    @Override
+    protected void stopMotorOutput() {
+        motor.stopMotor();
+    }
 
-  @Override
-  protected void setMotorFollow(BasicMotor master, boolean inverted) {
-    var motor = (BasicSparkMAX) master;
+    @Override
+    protected LogFrame.SensorData getSensorData() {
+        return null;
+    }
 
-    config.follow(motor.motor, inverted);
-    applyConfig();
+    @Override
+    protected LogFrame.PIDOutput getPIDLatestOutput() {
+        return LogFrame.PIDOutput.EMPTY;
+    }
 
-    this.motor.resumeFollowerMode();
-  }
+    @Override
+    public void setCurrentLimits(CurrentLimits currentLimits) {
+        // TODO: create a rev current limits class
+    }
 
-  @Override
-  protected void stopMotorFollow() {
-    motor.pauseFollowerMode();
-  }
+    @Override
+    public void setIdleMode(IdleMode mode) {
+        SparkBaseConfig.IdleMode value =
+                switch (mode) {
+                    case BRAKE -> SparkBaseConfig.IdleMode.kBrake;
+                    case COAST -> SparkBaseConfig.IdleMode.kCoast;
+                };
 
-  /** applies the current configuration to the motor */
-  private void applyConfig() {
-    // TODO: add error code handling for setting the configuration
-    motor.configure(
-        config,
-        SparkBase.ResetMode.kResetSafeParameters,
-        SparkBase.PersistMode.kNoPersistParameters);
-  }
+        config.idleMode(value);
+        applyConfig();
+    }
 
-  // TODO: add support for other rev encoders switching (like absolute encoders) directly
+    @Override
+    protected void setMotorPosition(double position) {
+        if (!(getMeasurements() instanceof MeasurementsREV encoder)) {
+            // TODO: add alert to say the encoder is not a rev encoder anymore
+            return;
+        }
+
+        encoder.setEncoderPosition(position);
+
+        motor.getClosedLoopController().setIAccum(0);
+    }
+
+    @Override
+    protected void setMotorFollow(BasicMotor master, boolean inverted) {
+        var motor = (BasicSparkMAX) master;
+
+        config.follow(motor.motor, inverted);
+        applyConfig();
+
+        this.motor.resumeFollowerMode();
+    }
+
+    @Override
+    protected void stopMotorFollow() {
+        motor.pauseFollowerMode();
+    }
+
+    /**
+     * applies the current configuration to the motor
+     */
+    private void applyConfig() {
+        // TODO: add error code handling for setting the configuration
+        motor.configure(
+                config,
+                SparkBase.ResetMode.kResetSafeParameters,
+                SparkBase.PersistMode.kNoPersistParameters);
+    }
+
+    // TODO: add support for other rev encoders switching (like absolute encoders) directly
 }
