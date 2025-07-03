3cf2303a2f420fea3a5eb18716b452de47b96c7c
captainsoccer <justeyallevi@gmail.com>
2025-07-03 14:53:04 +0300

    removed test motor

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,38 +4,11 @@
 
 package frc.robot;
 
-import edu.wpi.first.math.system.plant.DCMotor;
-import edu.wpi.first.wpilibj.RobotState;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
-import util.BasicMotor.BasicMotor;
-import util.BasicMotor.Configuration.BasicMotorConfig;
-import util.BasicMotor.Controllers.Controller.RequestType;
-import util.BasicMotor.Motors.Simulation.BasicSimElevator;
-
 public class RobotContainer {
   public RobotContainer() {
     configureBindings();
-
-    BasicMotorConfig config = new BasicMotorConfig();
-
-    config.motorConfig.gearRatio = 5;
-    config.motorConfig.name = "motor";
-    config.motorConfig.motorType = DCMotor.getKrakenX60(2);
-    config.simulationConfig.elevatorSimConfig.enableGravitySimulation = true;
-    config.simulationConfig.elevatorSimConfig.massKG = 20;
-    config.simulationConfig.elevatorSimConfig.pulleyRadiusMeters = 0.02;
-    config.simulationConfig.momentOfInertia = 0.05;
-
-    BasicMotor elevator = new BasicSimElevator(config);
-
-    new Trigger(RobotState::isEnabled)
-        .onTrue(new InstantCommand(() -> elevator.setReference(0, RequestType.VOLTAGE)));
-
-    SmartDashboard.putData(elevator.getController());
   }
 
   private void configureBindings() {}
